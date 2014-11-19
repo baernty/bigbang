e12fa2ee71c967a98524f065aacde9066b3e2928
Markus Schober <maxxscho@inode.at>
2014-11-19 16:50:58 +0100

    Adjustments (temporarily) to get the theme working

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'bigbang' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'bigbang' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'bigbang' ), 'bigbang', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'bigbang' ), 'bigbang', '<a href="http://www.markus-schober.at/" rel="designer">Markus Schober</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
